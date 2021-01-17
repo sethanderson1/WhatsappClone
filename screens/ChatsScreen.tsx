3e53f005433f61dd101a0b48f1333d57922853c3
Seth Anderson <sdande02@gmail.com>
2021-01-16 19:43:56 -0700

    amplify push

--- screens/ChatsScreen.tsx
+++ screens/ChatsScreen.tsx
@@ -5,15 +5,44 @@ import { View } from "../components/Themed";
 import ChatListItem from "../components/ChatListItem";
 import chatRooms from "../data/ChatRooms";
 import NewMessageButton from "../components/NewMessageButton";
+import { useEffect, useState } from "react";
+import { API, Auth, graphqlOperation } from "aws-amplify";
+import { getUser } from "./queries";
 
 export default function ChatsScreen() {
+  const [chatRooms, setChatRooms] = useState([])
+  
+  useEffect(() => {
+    const fetchChatRooms = async () => {
+      try {
+        const userInfo = await Auth.currentAuthenticatedUser();
+        
+        const userData = await API.graphql(
+          graphqlOperation(getUser, {
+            id: userInfo.attributes.sub,
+          })
+          );
+          
+          setChatRooms(userData.data.getUser.chatRoomUser.items)
+          
+          // console.log('chatRooms', chatRooms)
+        // console.log("userData", userData);
+
+      } catch (e) {
+        console.log("e", e);
+      }
+    };
+
+    fetchChatRooms();
+  }, []);
+
   return (
     <View style={styles.container}>
       <FlatList
         data={chatRooms}
-        renderItem={({ item }) => <ChatListItem chatRoom={item} />}
+        renderItem={({ item }) => <ChatListItem chatRoom={item.chatRoom} />}
         keyExtractor={(item) => item.id}
-        style={{ width: "100%", }}
+        style={{ width: "100%" }}
       />
       {/* <ChatListItem chatRoom={chatRooms[0]} /> */}
       <NewMessageButton />
